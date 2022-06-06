8e09fc4188ea2a572d891074f1c06ce9a0e3c685
Shihab Mahamud <shihab4t@gmail.com>
2022-06-07 04:23:28 +0600

    Implement redis service done

---
+++ src/services/redis.service.js
@@ -0,0 +1,21 @@
+const redisPackage = require("redis");
+
+class RedisService {
+    constructor(uri) {
+        this.client = redisPackage.createClient({ url: uri });
+        this.client.on("error", (err) => console.log("Redis not connected\n", err));
+        this.client.connect().then(() => console.log("Redis connected successfully"));
+    };
+    set = async function (key, value, ex = 3600) {
+        return await this.client.SET(key, value, { EX: ex });
+    };
+    get = async function (key) {
+        return await this.client.GET(key);
+    };
+    ttl = async function (key) {
+        return await this.client.TTL(key);
+    };
+};
+
+const redisService = new RedisService("redis://127.0.0.1:6379");
+module.exports = redisService;
